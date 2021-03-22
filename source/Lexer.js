4fe68d4aaa7b66f7ce18db8548032658fc687132
Marios F <greaperex@yahoo.com>
2021-03-22 23:27:01 +0200

    Fixed number parsing and refactored Lexer

--- source/Lexer.js
+++ source/Lexer.js
@@ -33,86 +33,94 @@ export default class Lexer {
     ];
 
     static parseLine(lineStr) {
-        let lexemes = [];
-        let token = "";
-        let insideQuote = false;
+        const nextLexeme = (lineStr) => {
+            let type, token = "";
+            let index = 0;
 
-        for (let i = 0; i < lineStr.length; ++i) {
-            if (!insideQuote && lineStr[i] === "\"") {
-                if (token !== "") {
-                    if (!isNaN(Number(token))) {
-                        lexemes.push({ type: this.TOKTYPES.NUMBER, value: Number(token) });
-                    } else {
-                        lexemes.push({ type: this.TOKTYPES.VARNAME, value: token });
-                    }
-                    token = "";
-                }
-                insideQuote = true;
-                continue;
+            while (lineStr[index].trim() === "") {
+                ++index;
+            }
+            if (index >= lineStr.length) {
+                return [ undefined, lineStr.substr(index) ];
             }
 
-            if (insideQuote) {
-                if (lineStr[i] === "\"") {
-                    lexemes.push({ type: this.TOKTYPES.STRING, value: token });
-                    token = "";
-                    insideQuote = false;
-                } else {
-                    token += lineStr[i];
+            if (lineStr[index] === '"') {
+                for (index = index + 1; index < lineStr.length && lineStr[index] !== '"'; ++index) {
+                    token += lineStr[index];
                 }
-                continue;
+                if (index === lineStr.length) {
+                    token += "\b";
+                }
+                type = this.TOKTYPES.STRING;
+                return [ { type: type, value: token }, lineStr.substr(index + 1) ];
             }
 
-            const c = lineStr[i].toUpperCase().trim();
-            if (c === "") {
-                if (token !== "") {
-                    if (!isNaN(Number(token))) {
-                        lexemes.push({ type: this.TOKTYPES.NUMBER, value: Number(token) });
-                    } else {
-                        lexemes.push({ type: this.TOKTYPES.VARNAME, value: token });
-                    }
-                    token = "";
-                }
-                continue;
-            }
-
-            const restOfLine = lineStr.substr(i);
+            const restOfLine = lineStr.substr(index);
             const foundKeyword = this.KEYWORDS.find((val) => restOfLine.startsWith(val));
+            if (foundKeyword) {
+                token = foundKeyword;
+                index += foundKeyword.length;
+                type = this.TOKTYPES.KEYWORD;
+                return [ { type: type, value: token }, lineStr.substr(index) ];
+            }
+
             const foundBuiltin = this.BUILTINS.find((val) => restOfLine.startsWith(val));
+            if (foundBuiltin) {
+                token = foundBuiltin;
+                index += foundBuiltin.length;
+                type = this.TOKTYPES.BUILTIN;
+                return [ { type: type, value: token }, lineStr.substr(index) ];
+            }
+
+            const re = /[+-]?\d*(\.\d*)?(E[+-]?\d+)?/i;
+            const found = restOfLine.match(re);
+            if (found && found[0] !== "" && found.index === 0) {
+                token = found[0];
+                index += found[0].length;
+                type = this.TOKTYPES.NUMBER;
+                return [ { type: type, value: Number(token) }, lineStr.substr(index) ];
+            }
+
             const foundOperator = this.OPERATORS.find((val) => restOfLine.startsWith(val));
-
-            if (foundKeyword || foundBuiltin || foundOperator) {
-                if (token !== "") {
-                    if (!isNaN(Number(token))) {
-                        lexemes.push({ type: this.TOKTYPES.NUMBER, value: Number(token) });
-                    } else {
-                        lexemes.push({ type: this.TOKTYPES.VARNAME, value: token });
-                    }
-                    token = "";
-                }
-
-                if (foundKeyword) {
-                    lexemes.push({ type: this.TOKTYPES.KEYWORD, value: foundKeyword });
-                    i += foundKeyword.length - 1;
-                } else if (foundBuiltin) {
-                    lexemes.push({ type: this.TOKTYPES.BUILTIN, value: foundBuiltin });
-                    i += foundBuiltin.length - 1;
-                } else if (foundOperator) {
-                    lexemes.push({ type: this.TOKTYPES.OPERATOR, value: foundOperator });
-                    i += foundOperator.length - 1;
-                }
-            } else {
-                token += c;
+            if (foundOperator) {
+                token = foundOperator;
+                index += foundOperator.length;
+                type = this.TOKTYPES.OPERATOR;
+                return [ { type: type, value: token }, lineStr.substr(index) ];
             }
-        }
-        if (token !== "") {
-            if (insideQuote) {
-                // TODO: Should I throw an error here?
-                lexemes.push({ type: this.TOKTYPES.STRING, value: token });
-            } else if (!isNaN(Number(token))) {
-                lexemes.push({ type: this.TOKTYPES.NUMBER, value: Number(token) });
-            } else {
-                lexemes.push({ type: this.TOKTYPES.VARNAME, value: token });
+
+            const keywordIndex = Math.min(... this.KEYWORDS.map((val) => {
+                const valIndex = restOfLine.indexOf(val);
+                return valIndex > -1 ? valIndex : restOfLine.length;
+            }));
+
+            const builtinIndex = Math.min(... this.BUILTINS.map((val) => {
+                const valIndex = restOfLine.indexOf(val);
+                return valIndex > -1 ? valIndex : restOfLine.length;
+            }));
+
+            const operatorIndex = Math.min(... this.OPERATORS.map((val) => {
+                const valIndex = restOfLine.indexOf(val);
+                return valIndex > -1 ? valIndex : restOfLine.length;
+            }));
+
+            let minIndex = Math.min(keywordIndex, builtinIndex, operatorIndex);
+
+            token = restOfLine.substr(0, minIndex).trim();
+            index += minIndex;
+            type = this.TOKTYPES.VARNAME;
+            return [ { type: type, value: token }, lineStr.substr(index) ];
+        };
+
+        let lexemes = [];
+        let lex, restOfLine = lineStr.toUpperCase();
+
+        while (restOfLine !== "") {
+            [ lex, restOfLine ] = nextLexeme(restOfLine);
+            if (!lex) {
+                break;
             }
+            lexemes.push(lex);
         }
 
         return lexemes;
